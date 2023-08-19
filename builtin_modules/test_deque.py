6423d6471601aa124cf92eb03c7cfcc3ab3433cc
Mason Lin <mason.lin1@hp.com>
2023-08-19 20:48:03 +0800

    stack and queue

--- builtin_modules/test_deque.py
+++ builtin_modules/test_deque.py
@@ -21,3 +21,23 @@ def test_all():
     assert queue[1] == 0
     assert queue[2] == 1
     assert queue[2] == 1
+
+    # queue
+    fifo = deque(maxlen=3)
+    fifo.append(1)
+    fifo.append(2)
+    fifo.append(3)
+    assert fifo.popleft() == 1
+    # fifo == [2, 3]
+    fifo.append(4)
+    fifo.append(5)
+    # fifo == [3, 4, 5]
+    assert fifo.popleft() == 3
+
+    # stack
+    lifo = deque(maxlen=3)
+    lifo.append(1)
+    lifo.append(2)
+    lifo.append(3)
+    assert lifo.pop() == 3
+    # lifo == [1, 2]
