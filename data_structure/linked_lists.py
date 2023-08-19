21d4fa5933a178b616348937c255b5ce6a7822fe
Mason Lin <mason.lin1@hp.com>
2023-08-19 17:22:49 +0800

    list

--- data_structure/linked_lists.py
+++ data_structure/linked_lists.py
@@ -1,31 +1,115 @@
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, data=None, next=None):
         self.data = data
-        self.next = None
+        self.next = next
 
 
 class LinkedList:
     def __init__(self):
-        self.root = None
+        self.head = None
+        self.tail = None
 
-    def add(self, data):
-        if self.root is None:
-            self.root = Node(data=data)
-        else:
-            current = self.root
-            while current.next is not None:
-                current = current.next
+    def append(self, data):
+        new_node = Node(data)
 
-    def delete(self, index):
-        if index == 0:
-            if self.root is not None:
-                self.root = self.root.next
-            else:
-                raise IndexError("Index out of range")
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+
         else:
-            current: Node = self.root
-            for _i in range(index):
-                if current.next is None:
-                    raise IndexError("Index out of range")
-                current = current.next
-            current.next = None
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
+    def delete(self):
+        if self.head is None:
+            return print("This list is empty")
+
+        if len(self) == 1:
+            self.head = None
+            return None
+
+        current_node = self.head
+        while current_node.next is not None:
+            self.tail = current_node
+            current_node = current_node.next
+        self.tail.next = None
+        return None
+
+    def insert(self, index, data):
+        """To insert the data to the specific index."""
+        if self.head is None:
+            raise ValueError("empty list")
+
+        if not 1 <= index <= len(self):
+            raise ValueError("index out of range")
+
+        if index == 1:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            cur_idx = 1
+            new_node = Node(data)
+            current_node = self.head
+            while cur_idx + 1 != index:
+                current_node = current_node.next
+                cur_idx += 1
+            new_node.next = current_node.next
+            current_node.next = new_node
+
+    def delete_node_by_index(self, index):
+        """To delete the specific node."""
+        if self.head is None:
+            raise ValueError("it's an empty list")
+
+        if index == 1 and len(self) > 1:
+            self.head = self.head.next
+
+        elif index == 1 and len(self) == 1:
+            self.head = None
+            self.tail = None
+
+        elif 1 < index < len(self):
+            cur_idx = 1
+            current_node = self.head
+            while cur_idx != index:
+                previous_node = current_node
+                current_node = current_node.next
+                cur_idx += 1
+            previous_node.next = current_node.next
+
+        elif index == len(self):
+            cur_idx = 1
+            current_node = self.head
+            while cur_idx != index:
+                previous_node = current_node
+                current_node = current_node.next
+                cur_idx += 1
+            previous_node.next = None
+            self.tail = previous_node
+
+        else:
+            raise ValueError("index out of range")
+
+    def reverse(self):
+        previous_node = None
+        current_node = self.head
+        self.tail = current_node
+        next_node = current_node.next
+
+        while next_node is not None:
+            current_node.next = previous_node
+            previous_node = current_node
+            current_node = next_node
+            next_node = next_node.next
+
+        current_node.next = previous_node
+        self.head = current_node
+
+    def __len__(self):
+        length = 0
+        current_node = self.head
+        while current_node is not None:
+            length += 1
+            current_node = current_node.next
+        return length
--- tests/test_data_structure.py
+++ tests/test_data_structure.py
@@ -3,12 +3,40 @@ from data_structure.linked_lists import LinkedList
 
 def test_linked_list_create():
     linked_list = LinkedList()
-    assert linked_list.root is None
+    assert linked_list.head is None
 
 
-def test_linked_list_add():
-    ...
+def test_linked_list_append():
+    linked_list = LinkedList()
+    linked_list.append("1")
+    assert linked_list.head.data == "1"
 
 
-def test_linked_list_delete():
-    ...
+def test_linked_list_delete_first():
+    linked_list = LinkedList()
+    linked_list.append("1")
+    linked_list.append("2")
+    linked_list.append("3")
+    linked_list.delete_node_by_index(1)
+    assert linked_list.head.data == "2"
+    assert linked_list.head.next.data == "3"
+
+
+def test_linked_list_delete_second():
+    linked_list = LinkedList()
+    linked_list.append("1")
+    linked_list.append("2")
+    linked_list.append("3")
+    linked_list.delete_node_by_index(2)
+    assert linked_list.head.data == "1"
+    assert linked_list.head.next.data == "3"
+
+
+def test_linked_list_delete_last():
+    linked_list = LinkedList()
+    linked_list.append("1")
+    linked_list.append("2")
+    linked_list.append("3")
+    linked_list.delete_node_by_index(3)
+    assert linked_list.head.data == "1"
+    assert linked_list.head.next.data == "2"
