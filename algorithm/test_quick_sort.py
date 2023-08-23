5225c9ec93d814b2987e89cd89eb105a33e769e4
Mason Lin <mason.lin1@hp.com>
2023-08-23 23:06:23 +0800

    q

--- algorithm/test_quick_sort.py
+++ algorithm/test_quick_sort.py
@@ -19,7 +19,29 @@ def quick_sort(arr, l, r):
         quick_sort(arr, p + 1, r)
 
 
+def quick_sort2(arr, head, tail):
+    l, r = head, tail
+    if l < r:
+        m = l + (r - l) // 2
+        pivot = arr[m]
+        while r >= l:
+            while r >= l and arr[l] < pivot:
+                l += 1
+            while r >= l and arr[r] > pivot:
+                r -= 1
+            if r >= l:
+                arr[l], arr[r] = arr[r], arr[l]
+                l += 1
+                r -= 1
+        quick_sort(arr, head, r)
+        quick_sort(arr, l, tail)
+
+
 def test_quick_sort():
     arr = [8, 7, 1, 4, 5, 2, 3, 6]
     quick_sort(arr, 0, len(arr) - 1)
+    assert arr == [1, 2, 3, 4, 5, 6, 7, 8]
+
+    arr = [8, 7, 1, 4, 5, 2, 3, 6]
+    quick_sort2(arr, 0, len(arr) - 1)
     assert arr == [1, 2, 3, 4, 5, 6, 7, 8]
