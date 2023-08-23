ad9c1c9fb083b148409cd8813cc70241c2d63d9a
Mason Lin <mason.lin1@hp.com>
2023-08-23 10:33:18 +0800

    gg

--- algorithm/test_binary_search.py
+++ algorithm/test_binary_search.py
@@ -1,16 +1,13 @@
 """[Python] Powerful Ultimate Binary Search Template. Solved many problems."""
 # https://leetcode.com/discuss/general-discussion/786126/Python-Powerful-Ultimate-Binary-Search-Template.-Solved-many-problems.
-from bisect import bisect_left, bisect_right
-
-search_space = [1, 2, 3, 4, 4, 4, 5, 6, 7]
 
 
 # Most Generalized Binary Search
-def binary_search(array) -> int:
+def binary_search(array, target) -> int:
     left, right = min(array), max(array)  # could be [0, n], [1, n] etc. Depends on problem
 
     def condition(mid) -> bool:
-        if array[mid] > 4:
+        if array[mid] > target:
             return True
         return False
 
@@ -23,6 +20,24 @@ def binary_search(array) -> int:
     return left
 
 
+# when miss
+def binary_search_with_miss(array, target) -> int:
+    left, right = min(array), max(array)  # could be [0, n], [1, n] etc. Depends on problem
+
+    def condition(mid) -> bool:
+        if array[mid] > target:
+            return True
+        return False
+
+    while left < right:
+        mid = (left + right) >> 1
+        if condition(mid):
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left if array[left] == target else -1
+
+
 # Tips:
 # Correctly initialize the boundary variables left and right to specify search space. Only one rule: set up the boundary to include all possible elements;
 # Decide return value. Is it return left or return left - 1? Remember this: after exiting the while loop, left is the minimal k​ satisfying the condition function;
@@ -33,11 +48,18 @@ def binary_search(array) -> int:
 
 
 def test_all():
-    search_space = [1, 2, 3, 4, 4, 4, 5, 6, 7]
+    # search_space = [1, 2, 3, 4, 4, 4, 5, 6, 7]
+    search_space = [1, 2, 3, 4, 4, 4, 6, 7, 8]
     #                        ^        ^
     #                        |        |
-    target = 4
-    assert bisect_left(search_space, target) == 3
+    target = 5
+    # assert bisect_left(search_space, target) == 3
 
-    assert bisect_right(search_space, target) == 6
-    assert binary_search(search_space) == 6
+    # assert bisect_right(search_space, target) == 6
+    # assert binary_search(search_space, target) == 6
+    # assert binary_search_with_miss(search_space, target) == 6
+
+    # assert bisect_left(search_space, target) == -1
+    # assert bisect_right(search_space, target) == -1
+    # assert binary_search(search_space, target) == -1
+    assert binary_search_with_miss(search_space, target) == -1
--- data_structure/test_graphs.py
+++ data_structure/test_graphs.py
@@ -1,12 +1,21 @@
-from collections import defaultdict
-
-
 class Graph:
     def __init__(self):
-        self.graph = defaultdict(list)
+        self.graph = {}
 
     def add_edge(self, from_vertex, to_vertex):
         self.graph[from_vertex].append(to_vertex)
 
     def add_vertex(self, vertex):
         self.graph[vertex] = []
+
+
+g = Graph()
+g.add_vertex(1)
+g.add_vertex(2)
+g.add_vertex(3)
+g.add_vertex(4)
+g.add_edge(1, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 3)
+
+print(g.graph)
