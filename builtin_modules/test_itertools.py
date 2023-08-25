d62613aee49e5dd2817844d772bc7a6d6fa2eb00
Mason Lin <mason.lin1@hp.com>
2023-08-25 14:28:05 +0800

    gg

--- builtin_modules/test_itertools.py
+++ builtin_modules/test_itertools.py
@@ -3,4 +3,15 @@ import itertools
 
 
 def test_all():
-    dir(itertools)
+    nums = [-1, 0, 1, 2, -1, -4]
+
+    from_itertools = []
+    for i, j in itertools.combinations(range(len(nums)), 2):
+        from_itertools.append([i, j])
+
+    from_for_loops = []
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            from_for_loops.append([i, j])
+
+    assert from_itertools == from_for_loops
