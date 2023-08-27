e2134abd408c9d22547401a4f8e1e2bea192e76b
Mason Lin <mason.lin1@hp.com>
2023-08-27 20:37:39 +0800

    a

--- quiz/test_4sum.py
+++ quiz/test_4sum.py
@@ -1,39 +1,33 @@
 # pylint: disable=C0200
 class Solution:
-    def fourSum(self, nums: list[int], four_sum_target: int) -> list[list[int]]:
-        # two pointers
+    def fourSum(self, nums: list[int], target: int) -> list[list[int]]:
         nums.sort()
-        results = []
-        four_sum_target = 0
+        result = []
 
         for i in range(len(nums) - 3):
-            if i == 0 or (i > 0 and nums[i - 1] != nums[i]):
-                three_sum_target = four_sum_target - nums[i]
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-                for j in range(i + 1, len(nums) - 2):
-                    if j == 0 or (j > 0 and nums[j - 1] != nums[j]):
-                        two_sum_target = three_sum_target - nums[j]
+            for j in range(i + 1, len(nums) - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
 
-                        # two sum
-                        left = i + 1
-                        right = len(nums) - 1
-                        while left < right:
-                            s = nums[left] + nums[right]
-                            if s == two_sum_target:
-                                results.append((nums[i], nums[j], nums[left], nums[right]))
-                                left += 1
-                                right -= 1
-                                # skip duplicate
-                                while left < right and nums[left] == nums[left - 1]:
-                                    left += 1
-                                while left < right and nums[right] == nums[right + 1]:
-                                    right -= 1
-                            elif s < two_sum_target:
-                                left += 1
-                            else:
-                                right -= 1
-
-        return results
+                left, right = j + 1, len(nums) - 1
+                while left < right:
+                    two_sum = nums[i] + nums[j] + nums[left] + nums[right]
+                    if two_sum == target:
+                        result.append((nums[i], nums[j], nums[left], nums[right]))
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif two_sum < target:
+                        left += 1
+                    else:
+                        right -= 1
+        return result
 
 
 def test_it():
