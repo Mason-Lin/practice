fb74a9d8cf72fa63d01c1c6a08e32260e808ba47
Mason Lin <mason.lin1@hp.com>
2023-08-27 23:30:36 +0800

    bfs

---
+++ algorithm/test_bfs.py
@@ -0,0 +1,44 @@
+# Python3 Program to print BFS traversal
+# from a given source vertex. BFS(int s)
+# traverses vertices reachable from s.
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left: Node = left
+        self.right: Node = right
+
+
+def bfs(head: Node):
+    queue = [head]
+    result = []
+    while queue:
+        node = queue.pop(0)
+        result.append(node.val)
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return result
+
+
+# Driver code
+def test_it():
+    # Create a graph given in
+    # the above diagram
+    #      0
+    #    /   \
+    #   1     2
+    #  / \   / \
+    # 3  4  5   6
+    head = Node(0)
+    head.left = Node(1)
+    head.right = Node(2)
+    head.left.left = Node(3)
+    head.left.right = Node(4)
+    head.right.left = Node(5)
+    head.right.right = Node(6)
+
+    # BFS traversal
+    assert bfs(head) == [0, 1, 2, 3, 4, 5, 6]
