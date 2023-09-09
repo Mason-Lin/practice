0516a71bad37c9951b30d93afac671e66149f264
Mason Lin <mason.lin1@hp.com>
2023-09-09 17:17:06 +0800

    a

---
+++ quiz/test_combinationSum.py
@@ -0,0 +1,27 @@
+from collections import Counter
+class Solution:
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+        seen = []
+        def backtracking(temp, result):
+            sum_temp = sum(temp)
+            if sum_temp == target:
+                comb = Counter(temp)
+                if comb not in seen:
+                    result.append([*temp])
+                    seen.append(comb)
+                return
+            if sum_temp > target:
+                return
+            for candidate in candidates:
+                temp.append(candidate)
+                backtracking(temp, result)
+                temp.pop()
+
+        result = []
+        temp = []
+        backtracking(temp, result)
+        return result
+
+
+def test_it():
+    assert Solution().combinationSum([2, 3, 6, 7], 7) == [[2, 2, 3], [7]]
