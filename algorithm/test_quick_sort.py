00dd2d12aa16cc0feda341d8131511e0d3aa28b5
Mason Lin <mason.lin1@hp.com>
2023-09-10 23:01:35 +0800

    a

--- algorithm/test_quick_sort.py
+++ algorithm/test_quick_sort.py
@@ -2,21 +2,21 @@
 
 
 # my best template for quick sort
-def partition(arr, l, r):
-    i, pivot = l, arr[r]
-    for j in range(l, r):
+def partition(arr, left, right):
+    i, pivot = left, arr[right]
+    for j in range(left, right):
         if arr[j] <= pivot:
             arr[i], arr[j] = arr[j], arr[i]
             i += 1
-    arr[i], arr[r] = arr[r], arr[i]
+    arr[i], arr[right] = arr[right], arr[i]
     return i
 
 
-def quick_sort(arr, l, r):
-    if l < r:
-        p = partition(arr, l, r)
-        quick_sort(arr, l, p - 1)
-        quick_sort(arr, p + 1, r)
+def quick_sort(arr, left, right):
+    if left < right:
+        pivot = partition(arr, left, right)
+        quick_sort(arr, left, pivot - 1)
+        quick_sort(arr, pivot + 1, right)
 
 
 def quick_sort2(arr, head, tail):
@@ -24,17 +24,17 @@ def quick_sort2(arr, head, tail):
     if l < r:
         m = l + (r - l) // 2
         pivot = arr[m]
-        while r >= l:
-            while r >= l and arr[l] < pivot:
+        while l <= r:
+            while l <= r and arr[l] < pivot:
                 l += 1
-            while r >= l and arr[r] > pivot:
+            while l <= r and arr[r] > pivot:
                 r -= 1
-            if r >= l:
+            if l <= r:
                 arr[l], arr[r] = arr[r], arr[l]
                 l += 1
                 r -= 1
-        quick_sort(arr, head, r)
-        quick_sort(arr, l, tail)
+        quick_sort2(arr, head, r)
+        quick_sort2(arr, l, tail)
 
 
 def test_quick_sort():
