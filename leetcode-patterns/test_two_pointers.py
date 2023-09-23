630437097ba7a5ab867596de33c4f6cef05ee87c
Mason Lin <mason.lin1@hp.com>
2023-09-23 22:53:57 +0800

    2p

--- leetcode-patterns/test_two_pointers.py
+++ leetcode-patterns/test_two_pointers.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 # 283. Move Zeroes
 class Solution283:
     def moveZeroes(self, nums: list[int]) -> None:
@@ -5,16 +8,82 @@ class Solution283:
         slow = fast = 0
         while fast < len(nums):
             if nums[fast] != 0:
-                nums[slow] = nums[fast]
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+            if nums[slow] != 0:
                 slow += 1
             fast += 1
-
-        while slow < len(nums):
-            nums[slow] = 0
-            slow += 1
 
 
 def test_283():
     nums = [0, 1, 0, 3, 12]
     Solution283().moveZeroes(nums)
     assert nums == [1, 3, 12, 0, 0]
+
+
+# 392. Is Subsequence
+class Solution392:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        slow, fast = 0, 0
+
+        if s in ("", t):
+            return True
+
+        while fast < len(t):
+            if s[slow] == t[fast]:
+                slow += 1
+            fast += 1
+
+            if slow == len(s):
+                return True
+        return False
+
+
+def test_392():
+    assert Solution392().isSubsequence(s="abc", t="ahbgdc") is True
+    assert Solution392().isSubsequence(s="axc", t="ahbgdc") is False
+
+
+# 11. Container With Most Water
+class Solution11:
+    def maxArea(self, height: list[int]) -> int:
+        left, right = 0, len(height) - 1
+
+        def calc_area(left, right):
+            return min(height[left], height[right]) * (right - left)
+
+        max_area = 0
+        while left < right:
+            max_area = max(max_area, calc_area(left, right))
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
+
+def test_11():
+    assert Solution11().maxArea(height=[1, 8, 6, 2, 5, 4, 8, 3, 7]) == 49
+    assert Solution11().maxArea(height=[1, 1]) == 1
+
+
+# 1679. Max Number of K-Sum Pairs
+class Solution1679:
+    def maxOperations(self, nums: list[int], k: int) -> int:
+        # time O(n) / space O(n)
+        seems = defaultdict(int)
+        count = 0
+        for num in nums:
+            looking = k - num
+            if seems[looking] > 0:
+                count += 1
+                seems[looking] = max(0, seems[looking] - 1)
+            else:
+                seems[num] += 1
+        return count
+
+
+def test_1679():
+    assert Solution1679().maxOperations(nums=[1, 2, 3, 4], k=5) == 2
+    assert Solution1679().maxOperations(nums=[3, 1, 3, 4, 3], k=6) == 1
---
+++ leetcode-patterns/test_two_pointers_linked_list.py
@@ -0,0 +1,141 @@
+import heapq
+from itertools import count
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+# 141. Linked List Cycle
+class Solution141:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+
+# 142. Linked List Cycle II
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = entry = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                while slow != entry:
+                    slow = slow.next
+                    entry = entry.next
+                return entry
+        return None
+
+
+# 160. Intersection of Two Linked Lists
+class Solution160:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        p = headA
+        q = headB
+        while p != q:
+            # if p is None then switch to headB
+            p = headB if not p else p.next
+            q = headA if not q else q.next
+        return p
+
+
+# 19. Remove Nth Node From End of List
+class Solution19:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        slow = fast = head
+        # move fast n steps ahead
+        for _ in range(n):
+            fast = fast.next
+
+        if not fast:
+            return head.next
+
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+        return head
+
+
+# 21. Merge Two Sorted Lists
+class Solution21:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        p = dummy = ListNode(-1)
+        p1 = list1
+        p2 = list2
+
+        while p1 and p2:
+            if p1.val < p2.val:
+                p.next = p1
+                p1 = p1.next
+            else:
+                p.next = p2
+                p2 = p2.next
+            p = p.next
+        if p1:
+            p.next = p1
+        if p2:
+            p.next = p2
+        return dummy.next
+
+
+# 23. Merge k Sorted Lists
+# not two pointer, but heap
+class Solution23:
+    def mergeKLists(self, lists: list[Optional[ListNode]]) -> Optional[ListNode]:
+        pq = []
+        cnt = count()
+        for head in lists:
+            if head:
+                heapq.heappush(pq, (head.val, next(cnt), head))
+        cur = dummy = ListNode(-1)
+
+        while len(pq) > 0:
+            node = heapq.heappop(pq)[2]
+            cur.next = node
+            cur = cur.next
+            if node.next:
+                heapq.heappush(pq, (node.next.val, next(cnt), node.next))
+
+        return dummy.next
+
+
+# 86. Partition List
+class Solution86:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        p1 = dummy1 = ListNode(-1)
+        p2 = dummy2 = ListNode(-1)
+        p = head
+
+        while p:
+            if p.val < x:
+                p1.next = p
+                p1 = p1.next
+            else:
+                p2.next = p
+                p2 = p2.next
+            p = p.next
+        p2.next = None
+        p1.next = dummy2.next
+        return dummy1.next
+
+
+# 876. Middle of the Linked List
+class Solution876:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
