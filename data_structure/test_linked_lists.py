9364e0996ef97292678b8b278e83f4883411bce0
Mason Lin <mason.lin1@hp.com>
2023-09-23 13:13:58 +0800

    a

--- data_structure/test_linked_lists.py
+++ data_structure/test_linked_lists.py
@@ -3,140 +3,246 @@ from typing import Optional
 
 
 class Node:
-    def __init__(self, val, next=None):
-        self.val = val
-        self.next: Optional[Node] = next
+    def __init__(self, val: int, next=None, random=None):
+        self.val = int(val)
+        self.next = next
+        self.random = random
 
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+def linked_list_to_array(linked_list: Node):
+    array = []
+    while linked_list:
+        array.append(linked_list.val)
+        linked_list = linked_list.next
+    return array
 
-    def append(self, data):
+
+def create_linked_list(data) -> Node:
+    dummy = Node(-1)
+    curr = dummy
+    for val in data:
+        curr.next = Node(val)
+        curr = curr.next
+    return dummy.next
+
+
+def insert(head: Node, index, data):
+    if head is None:
+        raise ValueError("empty list")
+
+    if index == 1:
         new_node = Node(data)
+        new_node.next = head
+        return new_node
 
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-
-        else:
-            self.tail.next = new_node
-            self.tail = self.tail.next
-
-    def delete(self):
-        if self.head is None:
-            return print("This list is empty")
-
-        if len(self) == 1:
-            self.head = None
-            return None
-
-        current_node = self.head
-        while current_node.next is not None:
-            self.tail = current_node
-            current_node = current_node.next
-        self.tail.next = None
-        return None
-
-    def insert(self, index, data):
-        """To insert the data to the specific index."""
-        if self.head is None:
-            raise ValueError("empty list")
-
-        if not 1 <= index <= len(self):
-            raise ValueError("index out of range")
-
-        if index == 1:
-            new_node = Node(data)
-            new_node.next = self.head
-            self.head = new_node
-        else:
-            cur_idx = 1
-            new_node = Node(data)
-            current_node = self.head
-            while cur_idx + 1 != index:
-                current_node = current_node.next
-                cur_idx += 1
-            new_node.next = current_node.next
-            current_node.next = new_node
-
-    def delete_node_by_index(self, index):
-        """To delete the specific node."""
-        if self.head is None:
-            raise ValueError("it's an empty list")
-
-        if index == 1 and len(self) > 1:
-            self.head = self.head.next
-
-        elif index == 1 and len(self) == 1:
-            self.head = None
-            self.tail = None
-
-        elif 1 < index < len(self):
-            cur_idx = 1
-            current_node = self.head
-            while cur_idx != index:
-                previous_node = current_node
-                current_node = current_node.next
-                cur_idx += 1
-            previous_node.next = current_node.next
-
-        elif index == len(self):
-            cur_idx = 1
-            current_node = self.head
-            while cur_idx != index:
-                previous_node = current_node
-                current_node = current_node.next
-                cur_idx += 1
-            previous_node.next = None
-            self.tail = previous_node
-
-        else:
-            raise ValueError("index out of range")
-
-    def reverse(self):
-        previous_node = None
-        current_node = self.head
-        self.tail = current_node
-        next_node = current_node.next
-
-        while next_node is not None:
-            current_node.next = previous_node
-            previous_node = current_node
-            current_node = next_node
-            next_node = next_node.next
-
-        current_node.next = previous_node
-        self.head = current_node
-
-    def __len__(self):
-        length = 0
-        current_node = self.head
-        while current_node is not None:
-            length += 1
-            current_node = current_node.next
-        return length
+    cur_idx = 1
+    new_node = Node(data)
+    current_node = head
+    while cur_idx + 1 != index:
+        current_node = current_node.next
+        cur_idx += 1
+    new_node.next = current_node.next
+    current_node.next = new_node
+    return head
 
 
-# 使用快慢指針找出中點
-def findMiddle(node):
-    fast = slow = node
+def delete(head: Node, index):
+    if head is None:
+        raise ValueError("empty list")
+
+    if index == 1:
+        return head.next
+
+    cur_idx = 1
+    current_node = head
+    while cur_idx + 1 != index:
+        current_node = current_node.next
+        cur_idx += 1
+    current_node.next = current_node.next.next
+    return head
+
+
+def length(head: Node):
+    if head is None:
+        raise ValueError("empty list")
+
+    length = 0
+    current_node = head
+    while current_node is not None:
+        length += 1
+        current_node = current_node.next
+    return length
+
+
+def find_middle(head: Node):
+    if head is None:
+        raise ValueError("empty list")
+
+    fast = slow = head
     while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
     return slow
 
 
-# 反轉linked list
-def reverseNode(node):
-    if not node or not node.next:
-        return node
+def reverse_node(head: Node):
+    # less than 2 nodes
+    if not head or not head.next:
+        return head
+
     prev = None
-    cur = node
-    while cur:
-        temp = cur
-        cur = cur.next
-        temp.next = prev
-        prev = temp
+    cur = head
+    while cur is not None:
+        nxt = cur.next
+        cur.next = prev
+        prev = cur
+        cur = nxt
     return prev
+
+
+def copy_random_list(head: Node) -> Optional[Node]:
+    if head is None:
+        return head
+
+    # insert cloned
+    node = head
+    while node:
+        original_next = node.next
+        # clone
+        cloned = Node(val=node.val, next=original_next, random=None)
+        node.next = cloned
+
+        # next
+        node = original_next
+
+    # copy random
+    node = head
+    while node:
+        cloned = node.next
+        # copy random
+        if node.random is not None:
+            cloned.random = node.random.next
+
+        # next
+        node = cloned.next
+
+    # split list
+    old_curr = head
+    new_curr = new_head = head.next
+    while old_curr:
+        old_curr.next = old_curr.next.next
+        if new_curr.next is not None:
+            new_curr.next = new_curr.next.next
+        old_curr = old_curr.next
+        new_curr = new_curr.next
+    return new_head
+
+
+def merge_two_lists_rec(list1: Node, list2: Node) -> Optional[Node]:
+    if list1 is None:
+        return list2
+    if list2 is None:
+        return list1
+
+    if list1.val < list2.val:
+        list1.next = merge_two_lists(list1.next, list2)
+        return list1
+    list2.next = merge_two_lists(list1, list2.next)
+    return list2
+
+
+def remove_duplicates(head: Node) -> Optional[Node]:
+    if head is None:
+        return head
+
+    cur = head
+    while cur:
+        while cur.next and cur.val == cur.next.val:
+            cur.next = cur.next.next
+        cur = cur.next
+    return head
+
+    # slow = fast = head
+    # while fast:
+    #     if fast.val != slow.val:
+    #         slow.next = fast
+    #         slow = slow.next
+    #     fast = fast.next
+    # slow.next = None
+    # return head
+
+
+def merge_two_lists(list1: Node, list2: Node) -> Optional[Node]:
+    new_node = dummy = Node(-1)
+    node_1 = list1
+    node_2 = list2
+
+    while node_1 and node_2:
+        if node_1.val < node_2.val:
+            new_node.next = node_1
+            node_1 = node_1.next
+        else:
+            new_node.next = node_2
+            node_2 = node_2.next
+        new_node = new_node.next
+    if node_1:
+        new_node.next = node_1
+    if node_2:
+        new_node.next = node_2
+    return dummy.next
+
+
+def test_linked_list():
+    # insert
+    linked_list = create_linked_list([1, 2, 3, 4, 5])
+    linked_list = insert(linked_list, index=1, data=9)
+    assert linked_list_to_array(linked_list) == [9, 1, 2, 3, 4, 5]
+
+    linked_list = create_linked_list([1, 2, 3, 4, 5])
+    linked_list = insert(linked_list, index=2, data=9)
+    assert linked_list_to_array(linked_list) == [1, 9, 2, 3, 4, 5]
+
+    # delete
+    linked_list = create_linked_list([1, 2, 3, 4, 5])
+    linked_list = delete(linked_list, index=1)
+    assert linked_list_to_array(linked_list) == [2, 3, 4, 5]
+
+    linked_list = create_linked_list([1, 2, 3, 4, 5])
+    linked_list = delete(linked_list, index=2)
+    assert linked_list_to_array(linked_list) == [1, 3, 4, 5]
+
+    # find length
+    linked_list = create_linked_list([1, 2, 3, 4, 5])
+    assert length(linked_list) == 5
+
+    # find middle
+    linked_list = create_linked_list([1, 2, 3, 4, 5])
+    assert find_middle(linked_list).val == 3
+    linked_list = create_linked_list([1, 2, 3, 4, 5, 6])
+    assert find_middle(linked_list).val == 4
+
+    # reverse
+    linked_list = create_linked_list([1, 2, 3, 4, 5])
+    linked_list = reverse_node(linked_list)
+    assert linked_list_to_array(linked_list) == [5, 4, 3, 2, 1]
+
+    # Copy List with Random Pointer
+    linked_list = create_linked_list([1, 2, 3, 4, 5])
+    cloned_linked_list = copy_random_list(linked_list)
+    assert linked_list_to_array(cloned_linked_list) == [1, 2, 3, 4, 5]
+    assert cloned_linked_list != linked_list
+
+    # Merge Two Sorted Lists
+    linked_list1 = create_linked_list([1, 2, 3, 4, 5])
+    linked_list2 = create_linked_list([6, 7, 8, 9])
+    merged_linked_list = merge_two_lists_rec(linked_list1, linked_list2)
+    assert linked_list_to_array(merged_linked_list) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    linked_list1 = create_linked_list([1, 2, 3, 4, 5])
+    linked_list2 = create_linked_list([6, 7, 8, 9])
+    merged_linked_list = merge_two_lists(linked_list1, linked_list2)
+    assert linked_list_to_array(merged_linked_list) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    # Remove Duplicates from Sorted List
+    linked_list = create_linked_list([1, 1, 2, 3, 3])
+    assert linked_list_to_array(remove_duplicates(linked_list)) == [1, 2, 3]
--- quiz/sliding_window.py
+++ quiz/sliding_window.py
@@ -1,10 +1,10 @@
 def slidingWindow(s: str):
     # 用合适的数据结构记录窗口中的数据
     window = {}
-    
+
     left = 0
     right = 0
-    
+
     while right < len(s):
         # c 是将移入窗口的字符
         c = s[right]
@@ -12,20 +12,24 @@ def slidingWindow(s: str):
             window[c] = 1
         else:
             window[c] += 1
-            
+
         # 增大窗口
         right += 1
-        
+
         # 进行窗口内数据的一系列更新
         # ...
-        
+
         # 判断左侧窗口是否要收缩
-        while left < right and window needs shrink:
+        while left < right and window_needs_shrink():
             # d 是将移出窗口的字符
-            d = s[left]
-            
+            s[left]
+
             # 缩小窗口
             left += 1
-            
+
             # 进行窗口内数据的一系列更新
             # ...
+
+
+def window_needs_shrink():
+    pass
