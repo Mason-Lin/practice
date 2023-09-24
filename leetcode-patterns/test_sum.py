7c7656f5c5022b6510853c588bdba5dc88e7a66c
Mason Lin <mason.lin1@hp.com>
2023-09-24 12:56:02 +0800

    a

--- leetcode-patterns/test_sum.py
+++ leetcode-patterns/test_sum.py
@@ -90,13 +90,11 @@ class Solution16:
         # two pointer
         nums.sort()
         closest = nums[0] + nums[1] + nums[2]
+        # closest = None
         for i in range(len(nums) - 2):
             left, right = i + 1, len(nums) - 1
             while left < right:
                 total = nums[i] + nums[left] + nums[right]
-                if total == target:
-                    return total
-
                 if abs(total - target) < abs(closest - target):
                     closest = total
 
@@ -104,6 +102,8 @@ class Solution16:
                     left += 1
                 elif total > target:
                     right -= 1
+                else:
+                    return total
 
         return closest
 
