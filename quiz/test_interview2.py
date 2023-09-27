8288fd3783b253a803be33a7dbfb383306f57019
Mason Lin <mason.lin1@hp.com>
2023-09-27 08:07:27 +0800

    a

---
+++ quiz/test_interview2.py
@@ -0,0 +1,30 @@
+import heapq
+from math import inf
+
+
+def find_largest_odd_integer(nums: list[int]) -> int:
+    maxi = -inf  # 9
+    heapq.heapify(nums)
+    for num in nums:  # [ 7, 9, 2, 10 ]
+        if num % 2 != 0:  # 7  9  2  10
+            maxi = max(maxi, num)
+    return maxi
+
+
+def find_largest_odd_integer2(nums: list[int], k: int) -> int:
+    # maxi = -inf  # 9
+    nums[:] = [-num for num in nums if num % 2 != 0]
+    heapq.heapify(nums)
+
+    for _ in range(k):
+        maxi = heapq.heappop(nums)
+    return -maxi
+
+
+def test_it():
+    assert find_largest_odd_integer([7, 9, 2, 10]) == 9
+    assert find_largest_odd_integer([7]) == 7
+
+    assert find_largest_odd_integer2([7, 9, 2, 10], 1) == 9
+    assert find_largest_odd_integer2([-1, 7, 9, 2, 10], 3) == -1
+    assert find_largest_odd_integer2([7], 1) == 7
