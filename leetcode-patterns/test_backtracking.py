d861455cac494d2bb0b3579a6f16278cefa39f13
Mason Lin <mason.lin1@hp.com>
2023-10-04 00:14:43 +0800

    aa

--- leetcode-patterns/test_backtracking.py
+++ leetcode-patterns/test_backtracking.py
@@ -5,6 +5,14 @@
 # https://labuladong.github.io/algo/di-san-zha-24031/bao-li-sou-96f79/hui-su-sua-89170/
 from collections import Counter
 
+"""
+訣竅:
+Subset: 用index避免走回頭路, 遇到有重複的數字, 跳過重複的數字
+Combination Sum: 用index避免走回頭路, 遇到有重複的數字, 跳過重複的數字, target紀錄剩餘的目標數字
+Permutations: 用一個 counter 來記錄每個數字出現的次數, 每次從counter選擇一個數字, 就把 counter 減一, 遞迴下去, 遞迴回來後, 再把 counter 加回來。
+"""
+
+
 BASE_CASE = None
 ITERATE_OVER_INPUT = range(10)
 OTHER_ARGUMENTS = None
@@ -28,154 +36,142 @@ def backtrack(curr, OTHER_ARGUMENTS):
 # 39. Combination Sum
 class Solution39:
     def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
-        self.nums = candidates
-        self.nums.sort()
-        self.ans = []
-        self.backtracking(target, 0, [])
-        return self.ans
+        res = []
 
-    def backtracking(self, target: int, index: int, path: list):
-        # add ans
-        if target == 0:
-            self.ans.append(path.copy())
-            return
-        # no ans, no need deeper
-        if target < 0:
-            return
-        # search deeper
-        for i in range(index, len(self.nums)):
-            path.append(self.nums[i])
-            self.backtracking(target - self.nums[i], i, path)
-            path.pop()
+        def backtracking(target, index, path):
+            if target == 0:
+                res.append(path.copy())
+                return
+            if target < 0:
+                return
+
+            for i in range(index, len(candidates)):
+                path.append(candidates[i])
+                backtracking(target - candidates[i], i, path)
+                path.pop()
+
+        backtracking(target, 0, [])
+        return res
 
 
 # 40. Combination Sum II
 class Solution40:
     def combinationSum2(self, candidates: list[int], target: int) -> list[list[int]]:
-        self.nums = candidates
-        self.nums.sort()
-        self.ans = []
-        self.backtracking(target, 0, [])
-        return self.ans
+        res = []
+        candidates.sort()
 
-    def backtracking(self, target: int, index: int, path: list) -> list:
-        if target == 0:
-            self.ans.append(path.copy())
-            return
-        if target < 0:
-            return
-        for i in range(index, len(self.nums)):
-            if i > index and self.nums[i] == self.nums[i - 1]:
-                continue
-            path.append(self.nums[i])
-            self.backtracking(target - self.nums[i], i + 1, path)
-            path.pop()
+        def backtracking(target, index, path):
+            if target == 0:
+                res.append(path.copy())
+                return
+            if target < 0:
+                return
+
+            for i in range(index, len(candidates)):
+                if i > index and candidates[i] == candidates[i - 1]:
+                    continue
+                path.append(candidates[i])
+                backtracking(target - candidates[i], i + 1, path)
+                path.pop()
+
+        backtracking(target, 0, [])
+        return res
 
 
 # 216. Combination Sum III
 class Solution216:
     def combinationSum3(self, k: int, n: int) -> list[list[int]]:
-        self.nums = list(range(1, 10))
-        self.size_limit = k
-        self.ans = []
-        self.backtracking(n, 0, [])
-        return self.ans
+        res = []
 
-    def backtracking(self, target, index, path):
-        if target == 0 and self.size_limit == len(path):
-            self.ans.append(path.copy())
-            return
-        if target < 0:
-            return
-        for i in range(index, len(self.nums)):
-            path.append(self.nums[i])
-            self.backtracking(target - self.nums[i], i + 1, path)
-            path.pop()
+        def backtracking(target, index, path):
+            if target == 0 and len(path) == k:
+                res.append(path.copy())
+                return
+            if target < 0:
+                return
+
+            for i in range(index, 10):
+                path.append(i)
+                backtracking(target - i, i + 1, path)
+                path.pop()
+
+        backtracking(n, 1, [])
+        return res
 
 
 # 46. Permutations
 class Solution46:
     def permute(self, nums: list[int]) -> list[list[int]]:
-        self.nums = nums
-        self.nums.sort()
-        self.ans = []
-        counter = Counter(self.nums)
-        self.backtracking(counter, [])
-        return self.ans
+        res = []
+        counter = Counter(nums)
 
-    def backtracking(self, counter, path):
-        # Base case
-        if len(self.nums) == len(path):
-            self.ans.append(path.copy())
-            return
+        def backtracking(path):
+            if len(path) == len(nums):
+                res.append(path.copy())
+            for num in counter:
+                if counter[num] > 0:
+                    path.append(num)
+                    counter[num] -= 1
+                    backtracking(path)
+                    path.pop()
+                    counter[num] += 1
 
-        for num in counter:
-            if counter[num] > 0:
-                path.append(num)
-                counter[num] -= 1
-                self.backtracking(counter, path)
-                path.pop()
-                counter[num] += 1
+        backtracking([])
+        return res
 
 
 # 47. Permutations II
 class Solution47:
     def permuteUnique(self, nums: list[int]) -> list[list[int]]:
-        self.nums = nums
-        self.nums.sort()
-        self.ans = []
-        counter = Counter(self.nums)
-        self.backtracking(counter, [])
-        return self.ans
+        res = []
+        counter = Counter(nums)
 
-    def backtracking(self, counter, path):
-        # Base case
-        if len(self.nums) == len(path):
-            self.ans.append(path.copy())
-            return
+        def backtracking(path):
+            if len(path) == len(nums):
+                res.append(path.copy())
+            for num in counter:
+                if counter[num] > 0:
+                    path.append(num)
+                    counter[num] -= 1
+                    backtracking(path)
+                    path.pop()
+                    counter[num] += 1
 
-        for num in counter:
-            if counter[num] > 0:
-                path.append(num)
-                counter[num] -= 1
-                self.backtracking(counter, path)
-                path.pop()
-                counter[num] += 1
+        backtracking([])
+        return res
 
 
 # 78. Subsets
 class Solution78:
     def subsets(self, nums: list[int]) -> list[list[int]]:
-        self.nums = nums
-        self.nums.sort()
-        self.ans = []
-        self.dfs(0, [])
-        return self.ans
+        res = []
 
-    def dfs(self, index, path):
-        self.ans.append(path.copy())
+        def backtracking(index, path):
+            res.append(path.copy())
 
-        for i in range(index, len(self.nums)):
-            path.append(self.nums[i])
-            self.dfs(i + 1, path)
-            path.pop()
+            for i in range(index, len(nums)):
+                path.append(nums[i])
+                backtracking(i + 1, path)
+                path.pop()
+
+        backtracking(0, [])
+        return res
 
 
 # 90. Subsets II
 class Solution90:
     def subsetsWithDup(self, nums: list[int]) -> list[list[int]]:
-        self.nums = nums
-        self.nums.sort()
-        self.ans = []
-        self.backtracking(0, [])
-        return self.ans
+        nums.sort()
+        res = []
 
-    def backtracking(self, index, path):
-        self.ans.append(path.copy())
+        def backtracking(index, path):
+            res.append(path.copy())
+            for i in range(index, len(nums)):
+                if i > index and nums[i] == nums[i - 1]:
+                    continue
+                path.append(nums[i])
+                backtracking(i + 1, path)
+                path.pop()
 
-        for i in range(index, len(self.nums)):
-            if i > index and self.nums[i] == self.nums[i - 1]:
-                continue
-            path.append(self.nums[i])
-            self.backtracking(i + 1, path)
-            path.pop()
+        backtracking(0, [])
+        return res
