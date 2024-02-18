9151efbb312d70cee529caae768da586d1d0abba
Mason Lin <pizza0117@gmail.com>
2024-02-18 12:38:36 +0800

    a

--- algorithm/test_prime.py
+++ algorithm/test_prime.py
@@ -10,3 +10,22 @@ def is_prime(x):
 def test_is_prime():
     assert is_prime(19) is True
     assert is_prime(91) is False
+
+
+def is_prime2(n):
+    if n <= 1:
+        return False
+    return all(n % i != 0 for i in range(2, int(n**0.5) + 1))
+
+
+def generate_primes(limit):
+    for num in range(2, limit):
+        if is_prime2(num):
+            yield num
+
+
+def test_prime_table():
+    prime_table = set()
+    for prime in generate_primes(100):
+        prime_table.add(prime)
+    assert prime_table == {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
